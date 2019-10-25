823da647d7ec79aa99e7ab3edca5424d337dc768
Andrei Micuda <andrei.micuda@gmail.com>
2019-10-25 23:10:04 +0300

    Create lab02_03.py

---
+++ Lab02/lab02_03.py
@@ -0,0 +1,18 @@
+'''
+Scrieți un program care să înlocuiască într-o propoziție toate aparițiile unui cuvânt 𝑠 cu un cuvânt 𝑡.
+'''
+
+str = input('sir: ')
+s = input('s: ')
+t = input('t: ')
+
+words = str.split()
+str_new = ''
+
+for x in words:
+    if s == x:
+        str_new += t + ' '
+    else:
+        str_new += x + ' '
+
+print(str_new)
