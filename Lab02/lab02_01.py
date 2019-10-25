f52f2775a0d52273b29d9b1001cad2404f462dcd
Andrei Micuda <andrei.micuda@gmail.com>
2019-10-25 22:59:29 +0300

    Create lab02_01.py

---
+++ Lab02/lab02_01.py
@@ -0,0 +1,10 @@
+'''
+Într-o propoziție/frază a fost efectuată, posibil de mai multe ori, aceeași greșeală de ortografie. Scrieți un program care citește propoziția, șirul greșit și șirul corect, după care afișează propoziția corectă. De exemplu, în propoziția “Problemele cu șiruri de caractedf nu sunt gdfle!” greșeală constă în faptul că în loc de șirul “re” a fost scris șirul “df”.
+'''
+
+s = input('s: ')
+old = input('gresit: ')
+new = input('corect: ')
+
+s = s.replace(old, new)
+print(s)
