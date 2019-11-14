ca43fd8f2b5734d61e1cdc6e74b140dbec3d8400
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 20:15:35 +0200

    Update lab01_06.py

--- Lab01/lab01_06.py
+++ Lab01/lab01_06.py
@@ -1,8 +1,6 @@
-'''
-Se citește un număr natural nenul n. Să se afișeze cel mai mic și cel mare
-număr ce pot fi formate din cifrele lui n. De exemplu, pentru n=812383 trebuie
-afișate numerele 883321 și 123388.
-'''
+#Se citește un număr natural nenul n. Să se afișeze cel mai mic și cel mare
+#număr ce pot fi formate din cifrele lui n. De exemplu, pentru n=812383 trebuie
+#afișate numerele 883321 și 123388.
 
 n = input('n: ')
 digits = list(n)
