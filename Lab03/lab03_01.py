79b9ceccd3f671c72211b69e9816351334415bfc
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 20:09:52 +0200

    Create lab03_01.py

---
+++ Lab03/lab03_01.py
@@ -0,0 +1,14 @@
+#Să se determine cele mai mari două valori distincte dintr-un șir de numere întregi.
+
+s = input()
+
+v = [int(x) for x in s.split()]
+
+mx1 = max(v)
+
+while mx1 in v:
+    v.remove(mx1)
+
+mx2 = max(v)
+
+print(mx1, mx2)
