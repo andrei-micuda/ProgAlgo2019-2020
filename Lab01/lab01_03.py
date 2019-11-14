f2c7e83b8a8f42e0a91815b112a5097062a34874
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 20:14:12 +0200

    Update lab01_03.py

--- Lab01/lab01_03.py
+++ Lab01/lab01_03.py
@@ -1,12 +1,10 @@
-'''
-Un meșter trebuie să paveze întreaga pardoseală a unei bucătării cu formă
-dreptunghiulară de dimensiune L_1×L_2 centimetri, cu plăci de gresie
-pătrate, toate cu aceeași dimensiune. Știind că meșterul nu vrea să taie nici o
-placă de gresie și vrea să folosească un număr minim de plăci, să se
-determine dimensiunea plăcilor de gresie de care are nevoie, precum și
-numărul lor. De exemplu, dacă L_1=440 cm și L_2=280 cm, atunci meșterul
-are nevoie de 77 de plăci de gresie, fiecare având latura de 40 cm.
-'''
+#Un meșter trebuie să paveze întreaga pardoseală a unei bucătării cu formă
+#dreptunghiulară de dimensiune L_1×L_2 centimetri, cu plăci de gresie
+#pătrate, toate cu aceeași dimensiune. Știind că meșterul nu vrea să taie nici o
+#placă de gresie și vrea să folosească un număr minim de plăci, să se
+#determine dimensiunea plăcilor de gresie de care are nevoie, precum și
+#numărul lor. De exemplu, dacă L_1=440 cm și L_2=280 cm, atunci meșterul
+#are nevoie de 77 de plăci de gresie, fiecare având latura de 40 cm.
 
 L_1 = int(input('L_1: '))
 L_2 = int(input('L_2: '))
