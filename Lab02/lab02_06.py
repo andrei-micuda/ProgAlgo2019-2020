15ef7471ecbc37625087a6717cb40ef8a202d634
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:43:57 +0200

    Update lab02_06.py

--- Lab02/lab02_06.py
+++ Lab02/lab02_06.py
@@ -1,9 +1,9 @@
-#Jurnalul electronic al Anei conține, în fiecare zi, câte o frază cu informații desprecheltuielile pe care ea le-a efectuat în ziua
-#respectivă. Scrieți un program care să citească o frază de acest tip din jurnalul Anei și apoi să afișeze suma totală cheltuită de
-#ea în ziua respectivă. De exemplu, pentru fraza “Astăzi am cumpărat pâine de 5 RON, pe lapte am dat 10 RON, iar de 15 RON am
-#cumpărat niște cașcaval. De asemenea, mi-am cumpărat și niște papuci cu 50 RON!”, programul trebuie să afișeze suma totală de
-#80 RON. Fraza se consideră corectă, adică toate numerele care apar în ea sunt numere naturale reprezentând sume cheltuite de Ana
-#în ziua respectivă!
+# Jurnalul electronic al Anei conține, în fiecare zi, câte o frază cu informații desprecheltuielile pe care ea le-a efectuat în ziua
+# respectivă. Scrieți un program care să citească o frază de acest tip din jurnalul Anei și apoi să afișeze suma totală cheltuită de
+# ea în ziua respectivă. De exemplu, pentru fraza “Astăzi am cumpărat pâine de 5 RON, pe lapte am dat 10 RON, iar de 15 RON am
+# cumpărat niște cașcaval. De asemenea, mi-am cumpărat și niște papuci cu 50 RON!”, programul trebuie să afișeze suma totală de
+# 80 RON. Fraza se consideră corectă, adică toate numerele care apar în ea sunt numere naturale reprezentând sume cheltuite de Ana
+# în ziua respectivă!
 
 s = input("Fraza: ")
 
