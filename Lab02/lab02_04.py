8228e9c5065f15b11bbb3ab0643d67fa4d1e46a5
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:44:32 +0200

    Update lab02_04.py

--- Lab02/lab02_04.py
+++ Lab02/lab02_04.py
@@ -1,6 +1,6 @@
-#Scrieți un program care să se verifice dacă două șiruri de caractere sunt anagrame sau nu. Două
-#șiruri sunt anagrame dacă unul se poate obține din celălalt printr-o permutare a caracterelor sale.
-#De exemplu, șirurile emerit și treime sunt anagrame, dar șirurile emerit și treimi nu sunt!
+# Scrieți un program care să se verifice dacă două șiruri de caractere sunt anagrame sau nu. Două
+# șiruri sunt anagrame dacă unul se poate obține din celălalt printr-o permutare a caracterelor sale.
+# De exemplu, șirurile emerit și treime sunt anagrame, dar șirurile emerit și treimi nu sunt!
 
 str1 = input('sir 1: ')
 str2 = input('sir 2: ')
