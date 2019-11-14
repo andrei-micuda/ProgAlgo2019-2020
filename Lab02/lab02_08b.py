b4f0a73bf8da3ed047c548ae82a78f22b20752d5
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:42:32 +0200

    Update lab02_08b.py

--- Lab02/lab02_08b.py
+++ Lab02/lab02_08b.py
@@ -1,11 +1,11 @@
-#Se citește de la tastatură un text în care cuvintele sunt despărțite în silabe cu ajutorul cratimelor. Se cere să se “traducă”
-#textul dat în limba păsărească astfel: după fiecare silabă se adaugă litera p și se repetă ultima literă din acea silabă.
-#Afișați traducerea și cu cratime, dar și fără.
-#Exemplu: “A-na a-re mul-te me-re ro-sii si de-li-cioa-se.” devine
-#“Apa-napa apa-repe mulpl-tepe mepe-repe ropo-siipi sipi depe-lipi-cioapa-sepe.” și
-#“Apanapa aparepe mulpltepe meperepe roposiipi sipi depelipicioapasepe.”
-#Fiind dat un astfel de text în limba păsărească (cel care conține și cratime), se poate obține textul original?
-#Dacă da, faceți asta.
+# Se citește de la tastatură un text în care cuvintele sunt despărțite în silabe cu ajutorul cratimelor. Se cere să se “traducă”
+# textul dat în limba păsărească astfel: după fiecare silabă se adaugă litera p și se repetă ultima literă din acea silabă.
+# Afișați traducerea și cu cratime, dar și fără.
+# Exemplu: “A-na a-re mul-te me-re ro-sii si de-li-cioa-se.” devine
+# “Apa-napa apa-repe mulpl-tepe mepe-repe ropo-siipi sipi depe-lipi-cioapa-sepe.” și
+# “Apanapa aparepe mulpltepe meperepe roposiipi sipi depelipicioapasepe.”
+# Fiind dat un astfel de text în limba păsărească (cel care conține și cratime), se poate obține textul original?
+# Dacă da, faceți asta.
 
 def reverse(s):
     rev = ""
