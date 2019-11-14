03e17811010c889b732946ee3ced411004014da8
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:46:12 +0200

    Update lab01_04.py

--- Lab01/lab01_04.py
+++ Lab01/lab01_04.py
@@ -1,9 +1,9 @@
-#Se citește un șir format din n numere reale strict pozitive (n≥2), reprezentând
-#cursul de schimb valutar RON/EURO din n zile consecutive. Să se afișeze
-#zilele între care a avut loc cea mai mare creștere a cursului valutar, precum și
-#cuantumul acesteia. De exemplu, pentru n=6 zile și cursul valutar dat de șirul
-#4.25,4.05,4.25,4.48,4.30,4.40, cea mai mare creștere a fost de 0.23 RON,
-#între zilele 3 și 4.
+# Se citește un șir format din n numere reale strict pozitive (n≥2), reprezentând
+# cursul de schimb valutar RON/EURO din n zile consecutive. Să se afișeze
+# zilele între care a avut loc cea mai mare creștere a cursului valutar, precum și
+# cuantumul acesteia. De exemplu, pentru n=6 zile și cursul valutar dat de șirul
+# 4.25,4.05,4.25,4.48,4.30,4.40, cea mai mare creștere a fost de 0.23 RON,
+# între zilele 3 și 4.
 
 
 n = int(input('n: '))
