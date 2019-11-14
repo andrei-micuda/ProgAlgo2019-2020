465ae1612348f1f67f850f8afe7a5bf55def45be
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:39:40 +0200

    Update lab02_07.py

--- Lab02/lab02_07.py
+++ Lab02/lab02_07.py
@@ -1,8 +1,6 @@
-'''
-Știind că 1 ianuarie 1702 a picat într-o zi de duminică, să se citească de la tastatură o dată mai recentă, și să se spună în ce
-zi a săptămânii cade aceasta. Puteți să faceți 2 cazuri - în care inputul este dat de forma "1 10 2019", sau "1 octombrie 2019". Folosiți funcția range() pentru a itera printre ani, respectiv instrucțiuni if/elif/else pentru a trata
-'''
-
+#Știind că 1 ianuarie 1702 a picat într-o zi de duminică, să se citească de la tastatură o dată mai recentă, și să se spună în ce
+#zi a săptămânii cade aceasta. Puteți să faceți 2 cazuri - în care inputul este dat de forma "1 10 2019", sau "1 octombrie 2019".
+#Folosiți funcția range() pentru a itera printre ani, respectiv instrucțiuni if/elif/else pentru a trata
 
 def isleap(year):
     year = int(year)
