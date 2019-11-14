2244f418023d39ba98e870d5b658df9caf1d9d51
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-14 21:46:45 +0200

    Update lab01_02.py

--- Lab01/lab01_02.py
+++ Lab01/lab01_02.py
@@ -1,11 +1,11 @@
-#Un greiere se deplasează efectuând câte o săritură, lungimea inițială a
-#săriturii fiind de x cm. După fiecare n sărituri, lungimea săriturii greierului se
-#micșorează cu p procente. Cunoscându-se valorile x,n,p, precum și numărul
-#de sărituri m pe care le face greierele, să se scrie un program care să afișeze
-#distanța parcursă de greiere. De exemplu, pentru x=20,n=10, p=10 și m=20
-#distanța parcursă de greiere este egală cu 380 cm, deoarece primele 10
-#sărituri efectuate au, fiecare, lungimea de 20 cm, iar următoarele 10 au,
-#fiecare, lungimea de 18 cm.
+# Un greiere se deplasează efectuând câte o săritură, lungimea inițială a
+# săriturii fiind de x cm. După fiecare n sărituri, lungimea săriturii greierului se
+# micșorează cu p procente. Cunoscându-se valorile x,n,p, precum și numărul
+# de sărituri m pe care le face greierele, să se scrie un program care să afișeze
+# distanța parcursă de greiere. De exemplu, pentru x=20,n=10, p=10 și m=20
+# distanța parcursă de greiere este egală cu 380 cm, deoarece primele 10
+# sărituri efectuate au, fiecare, lungimea de 20 cm, iar următoarele 10 au,
+# fiecare, lungimea de 18 cm.
 
 
 x = int(input('x: '))
