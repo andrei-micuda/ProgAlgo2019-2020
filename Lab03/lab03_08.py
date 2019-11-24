f24604e516a3eb6cfa5d9efd1b9171d45879db83
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2019-11-24 10:09:38 +0200

    Update lab03_08.py

--- Lab03/lab03_08.py
+++ Lab03/lab03_08.py
@@ -1,3 +1,14 @@
+# Magazine și produse
+# Fișierul text “inventar.txt” conține pe fiecare rând, cu spațiu între ele, un șir de litere reprezentând
+# numele unui magazin, urmat de numere naturale reprezentând codurile produselor aflate în stoc
+# la acel magazin.
+# a) Stocați informațiile citite din fișier într-un dicționar având ca chei numele magazinelor și ca
+# valori un set cu codurile produselor din acel magazin.
+# b) Afișați codurile acelor produse care există în stocul tuturor magazinelor (intersecție de seturi).
+# c) Afișați codurile tuturor produselor (reuniune de seturi).
+# d) Afișați pentru fiecare magazin: numele magazinului și codurile produselor exclusiviste (produse
+# care se află doar în acel magazin, nu și în altele) (diferență de seturi).
+
 fhandle = open("inventar.txt")
 
 data = fhandle.readlines()
@@ -5,16 +16,16 @@ data = fhandle.readlines()
 mag = dict()
 
 for entry in data:
-  entry = entry.split()
-  mag[entry[0]] = set()
-  for tmp in entry:
-    if tmp.isnumeric():
-      mag[entry[0]].add(tmp)
+    entry = entry.split()
+    mag[entry[0]] = set()
+    for tmp in entry:
+        if tmp.isnumeric():
+            mag[entry[0]].add(tmp)
 
 # a
 for k, v in mag.items():
-  print(k, end = ": ")
-  print(v)
+    print(k, end = ": ")
+    print(v)
 
 print()
 
@@ -22,7 +33,7 @@ print()
 union = set()
 
 for v in mag.values():
-  union = union.union(v)
+    union = union.union(v)
 
 print(union)
 print()
@@ -31,7 +42,7 @@ print()
 intersection = union.copy()
 
 for v in mag.values():
-  intersection = intersection.intersection(v)
+    intersection = intersection.intersection(v)
 
 print(intersection)
 print()
@@ -39,9 +50,9 @@ print()
 # d
 print("DIFF")
 for k, entry in mag.items():
-  diff = entry.copy()
-  print(k, end = ": ")
-  for v in mag.values():
-    if entry != v:
-      diff -= v
-  print(diff)
+    diff = entry.copy()
+    print(k, end = ": ")
+    for v in mag.values():
+        if entry != v:
+            diff -= v
+    print(diff)
